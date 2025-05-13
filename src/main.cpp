78fd1fcf32431a9c5bbc8069747b512e9e357b71
Kosteq95 <skostecki@tu.kielce.pl>
2025-05-13 17:23:45 +0200

    web_serwer_17_23

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,29 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncTCP.h>
+
+AsyncWebServer serwer(80);
+
 
 
 void Scan_Network(void);
+void Web_Page_Setup(void);
 
 void setup() {
   pinMode(2,OUTPUT);
-  WiFi.mode(WIFI_STA);
   Serial.begin(115200);
-  
+  WiFi.mode(WIFI_STA);
+  Serial.print("Moj adres MAC:"+ WiFi.macAddress() + "Rozpoczynam laczenie! ");
+  WiFi.begin("PAKISO_LAB","pakisolab");
+  while (WiFi.status() != WL_CONNECTED){
+      delay(500);
+      Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("Pomyslnie połączono! Otrzymałem IP: " + WiFi.localIP().toString());
+  Web_Page_Setup();
+
   
 }
 
@@ -39,6 +54,39 @@ void Scan_Network(void){
 
 }
 
+void Web_Page_Setup(void){
+  serwer.on("/",HTTP_GET,[](AsyncWebServerRequest *request){
+    String html = R"html(
+    <!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Strona Postawiona na ESP32</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <h5>Witaj świecie!</h5>
+        
+        <script src="" async defer></script>
+    </body>
+</html>
+    )html";
 
+
+    request->send(200,"text/html",html);
+  });
+
+  serwer.begin();
+}
 
 //TEST
